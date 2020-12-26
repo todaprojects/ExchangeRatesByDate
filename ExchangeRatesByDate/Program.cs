7a2da60f9846219b1924f2c687695b233d39a909
Tomas Daugela <todaprojects@gmail.com>
2020-12-26 19:55:04 +0200

    Deleted unnecessary using statements

--- ExchangeRatesByDate/Program.cs
+++ ExchangeRatesByDate/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
 using ExchangeRatesByDate.Helpers;
-using ExchangeRatesByDate.Printer;
 using ExchangeRatesByDate.Utils;
 
 namespace ExchangeRatesByDate
